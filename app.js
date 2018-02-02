87bf513307a0fc9e962d3a34077f8f0557e9486c
Marc Pomar <marc@faable.com>
2018-02-02 16:19:13 +0100

    auth demo

--- app.js
+++ app.js
@@ -7,22 +7,23 @@ const bodyParser = require('body-parser');
 const debug = require('debug')(`m2-0118-basic-auth:${path.basename(__filename).split('.')[0]}`);
 const mongoose = require("mongoose");
 const expressLayouts = require('express-ejs-layouts');
-
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
 const authRoutes = require('./routes/auth');
 const homeRoutes = require('./routes/home');
 
 const app = express();
 
-const {dbURL} = require('./config');
+const { dbURL } = require('./config');
 mongoose.connect(dbURL)
-        .then(() => debug(`Connected to DB ${dbURL}`))
-        .catch( e => debug(`CANNOT CONNECT TO DB`));
+  .then(() => debug(`Connected to DB ${dbURL}`))
+  .catch(e => debug(`CANNOT CONNECT TO DB`));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.set('layout','layout/main-layout');
+app.set('layout', 'layout/main-layout');
 app.use(expressLayouts);
 
 // uncomment after placing your favicon in /public
@@ -33,9 +34,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req,res,next) =>{
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+
+app.use((req, res, next) => {
   res.locals = {
-    title: 'DEFAULT'
+    title: 'DEFAULT',
+    user: req.session.currentUser || null
   }
   next();
 });
@@ -44,14 +55,14 @@ app.use('/', homeRoutes);
 app.use('/auth', authRoutes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/home.js
+++ routes/home.js
@@ -5,4 +5,11 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Home' });
 });
 
+router.get('/private', function(req, res, next) {
+    if(req.session.currentUser){
+        res.render('private', { title: 'Private' });
+    }
+    res.redirect('http://disney.com');
+});
+
 module.exports = router;
